9e48446d9bc693e55c743e062027d507f34eb3e2
GioeleB00 <gioelebotta00@gmail.com>
2025-07-03 15:23:05 +0200

    minor correction

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.config.settings import settings
 from app.db.base import Base
-import app.models.table_users
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.

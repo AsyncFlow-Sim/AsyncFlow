da5cbd540c934981fa7a3558ceb16fa18ab960be
GioeleB00 <gioelebotta00@gmail.com>
2025-09-08 23:41:49 +0200

    mmc split + notebook mmc split + random lb algo

--- src/asyncflow/analysis/__init__.py
+++ src/asyncflow/analysis/__init__.py
@@ -3,5 +3,6 @@
 from asyncflow.metrics.simulation_analyzer import ResultsAnalyzer
 from asyncflow.metrics.sweep_analyzer import SweepAnalyzer
 from asyncflow.queue_theory_analysis.mm1 import MM1
+from asyncflow.queue_theory_analysis.mmc import MMc
 
-__all__ = ["MM1", "ResultsAnalyzer", "SweepAnalyzer"]
+__all__ = ["MM1", "MMc", "ResultsAnalyzer", "SweepAnalyzer"]
